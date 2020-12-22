f4fa6a6a36516d39fb5bccf457381fd47fdb2be6
rhidalgocipfpbatoi <rhidalgo@cipfpbatoi.es>
2020-12-22 16:25:52 +0100

    Version Practica 3.5

--- src/Practica31.java
+++ src/Practica31.java
@@ -4,6 +4,10 @@ public class Practica31 {
 
     private static final Scanner scanner = new Scanner(System.in);
     private static int topeNota;
+    private static final int SUSPENSO = 0;
+    private static final int APROBADO = 1;
+    private static final int NOTABLE = 2;
+    private static final int EXCELENTE = 3;
 
     public static int numeroNotas() {
         System.out.println("¿Cuántas notas vas a meter?");
@@ -17,7 +21,7 @@ public class Practica31 {
         int i;
         // Introducir las notas
         for (i = 0; i < numNotasAPedir; i++) {
-            System.out.println("Introduce la nota " + (i+1));
+            System.out.println("Introduce la nota " + (i + 1));
             notas[i] = scanner.nextDouble();
             if (notas[i] > 10 || notas[i] < 0) {
                 if (notas[i] == -1) {
@@ -41,7 +45,7 @@ public class Practica31 {
         }
     }
 
-    public static void muestraMedia(double [] notas) {
+    public static void muestraMedia(double[] notas) {
         double suma = 0;
         for (int i = 0; i < topeNota; i++) {
             suma += notas[i];
@@ -52,12 +56,12 @@ public class Practica31 {
         System.out.printf("La nota media es %.2f\n", media);
     }
 
-    public static void muestraNotaMaxima(double [] notas) {
+    public static void muestraNotaMaxima(double[] notas) {
         double notaMaxima = Double.MIN_VALUE;
 
         for (int i = 0; i < topeNota; i++) {
 
-            if (notas[i]>notaMaxima) {
+            if (notas[i] > notaMaxima) {
                 notaMaxima = notas[i];
             }
         }
@@ -72,7 +76,7 @@ public class Practica31 {
 
         for (int i = 0; i < topeNota; i++) {
             if (notaABuscar == notas[i]) {
-                System.out.println("La nota está en la posición " + (i+1));
+                System.out.println("La nota está en la posición " + (i + 1));
                 return;
             }
         }
@@ -80,13 +84,56 @@ public class Practica31 {
         System.out.printf("La nota %.2f no existe", notaABuscar);
     }
 
+    public static int[] resumenCalificaciones(double[] notas) {
+        int[] resumenCalificaciones = new int[4];
+
+        for (int i = 0; i < topeNota; i++) {
+            if (notas[i] >= 0 && notas[i] < 5) {
+                resumenCalificaciones[SUSPENSO]++;
+            } else if (notas[i] >= 5 && notas[i] < 7) {
+                resumenCalificaciones[APROBADO]++;
+            } else if (notas[i] >= 7 && notas[i] < 9) {
+                resumenCalificaciones[NOTABLE]++;
+            } else {
+                resumenCalificaciones[EXCELENTE]++;
+            }
+        }
+
+        return resumenCalificaciones;
+    }
+
+    public static void muestraResultado(
+            int[] calificaciones,
+            String tituloResultado, int idCalificacion) {
+
+        System.out.println(tituloResultado);
+        for (int i = 0; i < calificaciones[idCalificacion]; i++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+
+    public static void muestraGraficaNotas(double[] notas) {
+
+        int[] calificaciones = resumenCalificaciones(notas);
+
+        System.out.println("Gráfico de barras de las notas");
+        System.out.println("------------------------------");
+
+        muestraResultado(calificaciones, "Suspendidos", SUSPENSO);
+        muestraResultado(calificaciones, "Aprobados", APROBADO);
+        muestraResultado(calificaciones, "Notables", NOTABLE);
+        muestraResultado(calificaciones, "Excelentes", EXCELENTE);
+    }
+
     public static void main(String[] args) {
 
         int numNotas = numeroNotas();
-        double [] notas = pedirNotas(numNotas);
+        double[] notas = pedirNotas(numNotas);
         mostrarNotas(notas);
         muestraMedia(notas);
         muestraNotaMaxima(notas);
-        buscarNota(notas);
+        //buscarNota(notas);
+        muestraGraficaNotas(notas);
     }
 }
